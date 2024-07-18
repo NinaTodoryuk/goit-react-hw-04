71c8b20598e584bf027aad189a0d93fb7a662bdb
NinaTodoryuk <nina.todoryuk@gmail.com>
2024-07-17 20:43:46 -1200

    fear

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,8 @@
+import css from './ErrorMessage.module.css'
+
+export default function ErrorMessage({message}) 
+{ return 
+    <div className={css.error}>
+        {message}
+    </div>;
+}
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import css from './Loader.module.css'
+import { FallingLines } from "react-loader-spinner";
+
+export default function Loader() {
+    return (
+      <div className={css.backdrop}>
+        <FallingLines position='center'/>;
+      </div>
+    );    
+}
